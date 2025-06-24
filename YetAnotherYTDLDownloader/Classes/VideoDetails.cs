adeb79e2fec1738b07bc52f1144330e06f0acf37
Louis <nguyenlouis4@gmail.com>
2025-06-24 11:38:25 +1000

    Added formatted string property to better show format to user.

--- YetAnotherYTDLDownloader/Classes/VideoDetails.cs
+++ YetAnotherYTDLDownloader/Classes/VideoDetails.cs
@@ -13,6 +13,13 @@ namespace YetAnotherYTDLDownloader.Classes
 {
 	public class VideoDetails
 	{
+		//only used for debug and layout purposese
+		public VideoDetails(string id, string title)
+		{ 
+			ID = id;
+			Title = title; 
+		}
+
 		[JsonPropertyName("id")]
 		public string ID { get; set; } = "";
 		[JsonPropertyName("title")]
@@ -32,26 +39,32 @@ namespace YetAnotherYTDLDownloader.Classes
 		public string LiveStatus { get; set; } = "";
 		[JsonPropertyName("formats")]
 		public List<VideoFormatDetails>? Formats { get; set; } = null;
-
-
 	}
 
 	public class VideoFormatDetails
 	{
 		[JsonPropertyName("format_id")]
 		public string FormatID { get; set; } = "";
+
 		[JsonPropertyName("fps")]
 		public double FPS { get; set; }
+
 		[JsonPropertyName("width")]
 		public int Width { get; set; }
+
 		[JsonPropertyName("height")]
 		public int Height { get; set; }
+
 		[JsonPropertyName("resolution")]
 		public string Resolution { get; set; } = "";
+
 		[JsonPropertyName("video_ext")]
 		public string VideoExt { get; set; } = "";
+
 		[JsonPropertyName("audio_ext")]
 		public string AudioExt { get; set; } = "";
+
+		public String FormattedText{ get => $"{FormatID}: {Resolution}.{VideoExt}"; }
 	}
 
 }
