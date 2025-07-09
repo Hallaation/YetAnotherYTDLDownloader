c0ad770cbf60b55218ff97fdda3f6f4a99509b88
Louis <nguyenlouis4@gmail.com>
2025-07-09 16:48:08 +1000

    Hooking up download button to build args and run the necessary process. Moved YTDLPArgs into member variable, providing view access to the settings. Removing unused SelectedVideo property. WIP progress updating.

--- YetAnotherYTDLDownloader/MainWindow.xaml.cs
+++ YetAnotherYTDLDownloader/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Security.Policy;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -37,24 +40,12 @@ namespace YetAnotherYTDLDownloader
 
 	public class MainWindowViewModel : PropertyNotifiable
 	{
+		Regex downloadRgx = new Regex("[0-9]{0,3}%");
 		public string InputURL { get; set; } = "";
-
 		public ObservableCollection<VideoDetails> VideoDetailList { get; set; } = new ObservableCollection<VideoDetails>();
-
-		//public VideoDetails? SelectedVideo
-		//{
-		//	get
-		//	{
-		//		if (SelectedURLIndex > -1 && SelectedURLIndex < VideoDetailList.Count)
-		//		{
-		//			return VideoDetailList[SelectedURLIndex];
-		//		}
-		//		return null;
-		//	}
-		//}
+		public YTDLPArgs DownloadArgs { get; set; } = new YTDLPArgs();
 
 		public VideoDetails? SelectedVideo { get; private set; } = null;
-
 
 		private int mSelectedURLIndex = 0;
 		public int SelectedURLIndex
@@ -68,6 +59,9 @@ namespace YetAnotherYTDLDownloader
 				//Trace.WriteLine($"Selected URL Index changed to {mSelectedURLIndex}");
 			}
 		}
+		public float DownloadProgress { get; set; } = 0.0f;
+		public int SelectedVideoFormatIdx { get; set; } = -1;
+		public int SelectedAudioFormatIdx { get; set; } = -1;
 
 		public SimpleCommand AnalyzeAndAdd
 		{
@@ -77,12 +71,24 @@ namespace YetAnotherYTDLDownloader
 			});
 		}
 
+
+		public SimpleCommand StartDownload
+		{
+			get => new SimpleCommand(ex =>
+			{
+				this.DownloadVideo(InputURL);
+			});
+		}
+
 		private void AnalyzeVideo(string url)
 		{
 			//Build my arguments
 			YTDLPHandler handler = new YTDLPHandler();
-			YTDLPArgs yTDLPArgs = new YTDLPArgs(url) { AnalyzeMode = true };
-			handler.Args = yTDLPArgs.Args;
+
+			DownloadArgs.URL = url;
+			DownloadArgs.AnalyzeMode = true;
+
+			handler.Args = DownloadArgs.BuildArgs();
 			VideoDetails? outVideoDets = null;
 			Trace.WriteLine($"Starting from thread{Thread.CurrentThread.ManagedThreadId}");
 
@@ -90,7 +96,6 @@ namespace YetAnotherYTDLDownloader
 			{
 				try
 				{
-					
 					outVideoDets = JsonSerializer.Deserialize<VideoDetails>(stdout);
 					if (outVideoDets != null)
 					{
@@ -100,12 +105,69 @@ namespace YetAnotherYTDLDownloader
 							//VideoDetailList.Add(outVideoDets);
 							SelectedVideo = outVideoDets;
 							Trace.Assert(!string.IsNullOrEmpty(outVideoDets.ID));
-							InputURL = "";
 							Notify(nameof(SelectedVideo));
 							//Notify(nameof(VideoDetailList));
 						});
 					}
 
+				}
+				catch (Exception ex)
+				{
+					Trace.WriteLine(ex.ToString());
+				}
+			});
+		}
+
+		private void DownloadVideo(string url)
+		{
+			YTDLPHandler handler = new YTDLPHandler();
+
+			DownloadArgs.URL = url;
+			DownloadArgs.AnalyzeMode = false;
+
+			//get the video format
+			if (SelectedVideoFormatIdx != -1)
+			{
+				if (SelectedVideo != null && SelectedVideo?.VideoFormats?.Count > 0)
+				{
+					DownloadArgs.SelectedAudioFormatID = SelectedVideo.VideoFormats[SelectedVideoFormatIdx].FormatID;
+					//DownloadArgs.SelectedFormatID = SelectedVideo.Formats[SelectedFormatIdx].FormatID;
+				}
+			}
+			//get the audio format
+			if (SelectedAudioFormatIdx != -1)
+			{
+				if (SelectedVideo != null && SelectedVideo?.AudioFormats?.Count > 0)
+				{
+					DownloadArgs.SelectedAudioFormatID = SelectedVideo.AudioFormats[SelectedAudioFormatIdx].FormatID;
+					//DownloadArgs.SelectedFormatID = SelectedVideo.Formats[SelectedFormatIdx].FormatID;
+				}
+			}
+			if (SelectedVideo?.AudioFormats?.Count > 0 && SelectedAudioFormatIdx < 0)
+			{
+				//Audio not selected but audio formats exist, do we want to do anything?
+			} 
+
+			handler.Args = DownloadArgs.BuildArgs();
+
+			Action<string> stdallProcess = (string stdall) => { };
+			handler.Exec(stdallProcess, stdout =>
+			{
+				try
+				{
+					Application.Current.Dispatcher.BeginInvoke(() =>
+					{
+						//Match? progressMatch = downloadRgx.Match(stdout);
+						//if (progressMatch != null && progressMatch.Groups.Count > 0)
+						//{
+							//string progress = progressMatch.Groups[0].Value;
+							//progress.Remove('%');
+							//if (!string.IsNullOrEmpty(progress))
+							//{
+							//	this.DownloadProgress = float.Parse(progress);
+							//}
+						//}
+					});
 				}
 				catch (Exception ex)
 				{
