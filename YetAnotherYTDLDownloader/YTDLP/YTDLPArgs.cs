33db6c30e54ef2b6a4d2d5fee8fcf8c884c3be61
Louis <nguyenlouis4@gmail.com>
2025-07-09 16:45:51 +1000

    Moved WaitForVideoTimeInSeconds above to group bools together. Adding selected video and format IDs to be used when downloading. Changed Args from a property to function BuildArgs making it more clear as to what it does. Added missing embed-thumbnail hookup. Removed overloaded ctor taking downloadURL, made URL public property. Removed unused overloaded ctor

--- YetAnotherYTDLDownloader/YTDLP/YTDLPArgs.cs
+++ YetAnotherYTDLDownloader/YTDLP/YTDLPArgs.cs
@@ -10,88 +10,105 @@ namespace YetAnotherYTDLDownloader.YTDLP
 {
 	public class YTDLPArgs
 	{
-		public YTDLPArgs(string downloadURL)
-		{
-			mURL = downloadURL;
-		}
-		public YTDLPArgs(string videoFormatID, string audioFormatID)
-		{
-			
-		}
 
 		public string OutputDir { get; set; } = Directory.GetCurrentDirectory();
 
 		public bool AnalyzeMode { get; set; } = false;
 
+		public int WaitForVideoTimeInSeconds { get; set; } = 30; //30 by default
 		public bool VerboseOutput { get; set; } = true;
 		public bool WaitForVideo { get; set; } = false;
-		public int WaitForVideoTimeInSeconds { get; set; } = 30; //30 by default
-
 		public bool LiveFromStart { get; set; } = false;
 
 		public bool EmbedMetadata { get; set; } = true;
 		public bool EmbedThumbnail { get; set; } = true;
-
 		public bool SplitTracks { get; set; } = false;
+		public bool IncludeUploader { get; set; } = true;
 
-		private string mURL = "";
+		public string SelectedVideoFormatID { get; set; } = "";
+		public string SelectedAudioFormatID { get; set; } = "";
 
-		public String Args 
-		{ 
-			get 
+		public string URL { get; set; } = "";
+
+		public String BuildArgs()
+		{
+			Trace.Assert(URL != null || !string.IsNullOrEmpty(URL));
+
+			List<string> args = new List<string>();
+
+			if (AnalyzeMode)
 			{
-				Trace.Assert(mURL != null || !string.IsNullOrEmpty(mURL));
+				args.Add("-j");
+				string ret = args.Aggregate((curr, next) => $"{curr} {next} ");
+				ret += " " + URL;
+				return ret;
 
-				List<string> args = new List<string>(); 
-				
-				if (AnalyzeMode)
-				{
-					args.Add("-j");
-					string ret = args.Aggregate((curr, next) => $"{curr} {next} ");
-					ret += " " + mURL;
-					return ret;
-
-				}
-				#region downloadArgs	
-				///-------------------------------------------------------- Download Args -------------------------------------------------------------------
-				if (VerboseOutput)
-				{
-					args.Add("-v");
-				}
-
-				if (WaitForVideo && WaitForVideoTimeInSeconds > 0)
-				{
-					args.Add($"--wait-for-video {WaitForVideoTimeInSeconds}");
-				}
-
-				if (LiveFromStart)
-				{
-					args.Add("--live-from-start");
-				}
-
-				if (EmbedMetadata)
-				{
-					args.Add("--embed-metadata");
-				}
-
-				if (!SplitTracks)
-				{
-					args.Add("--no-split-chapters");
-				}
-
-				//add any arguments we want by default
-				//avoids corruption, don't think we need an option for this
-				args.Add("--hls-use-mpegts");
-				args.Add("--progress");
-
-				args.Add($"-o {OutputDir}/%(uploader)s/%(uploader%s-%(title)s.%(ext)s");
-
-				string temp = "";
-				temp = args.Aggregate( (curr, next) => $"{curr} {next}" );
-				temp += " " + mURL;
-				return temp;
-				#endregion
 			}
+			#region downloadArgs	
+			///-------------------------------------------------------- Download Args -------------------------------------------------------------------
+			if (VerboseOutput)
+			{
+				args.Add("-v");
+			}
+
+			if (!string.IsNullOrEmpty(SelectedVideoFormatID))
+			{
+				if (string.IsNullOrEmpty(SelectedAudioFormatID))
+				{
+					args.Add($"-f {SelectedVideoFormatID}+{SelectedAudioFormatID}");
+				}
+				//only video selected
+				else
+				{
+					args.Add($"-f {SelectedVideoFormatID}");
+				}
+			}
+
+			if (WaitForVideo && WaitForVideoTimeInSeconds > 0)
+			{
+				args.Add($"--wait-for-video {WaitForVideoTimeInSeconds}");
+			}
+
+			if (LiveFromStart)
+			{
+				args.Add("--live-from-start");
+			}
+
+			if (EmbedMetadata)
+			{
+				args.Add("--embed-metadata");
+			}
+
+			if (EmbedThumbnail)
+			{
+				args.Add("--embed-thumbnail");
+			}
+
+			if (!SplitTracks)
+			{
+				args.Add("--no-split-chapters");
+			}
+
+
+			//add any arguments we want by default
+			//avoids corruption, don't think we need an option for this
+			args.Add("--hls-use-mpegts");
+			args.Add("--progress");
+
+			if (IncludeUploader)
+			{
+				args.Add($"-o {OutputDir}\\%(uploader)s\\%(uploader)s-%(title)s.%(ext)s");
+			}
+			else
+			{
+				args.Add($"-o {OutputDir}\\%(title)s.%(ext)s");
+			}
+			string temp = "";
+			temp = args.Aggregate((curr, next) => $"{curr} {next}");
+			temp += " " + URL;
+			return temp;
+			#endregion
+
 		}
 
 	}
