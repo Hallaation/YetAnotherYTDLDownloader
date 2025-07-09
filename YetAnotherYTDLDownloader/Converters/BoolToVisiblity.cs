302f4042052eb2ed47a1289459cf0260681a46ef
Louis <nguyenlouis4@gmail.com>
2025-07-09 16:43:49 +1000

    Adding bool to visibility converters

---
+++ YetAnotherYTDLDownloader/Converters/BoolToVisiblity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Navigation;
+
+namespace YetAnotherYTDLDownloader.Converters
+{
+	internal class BoolToVisiblity : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			//bool isboolvalue = (value is bool) && isboolvalue == true
+			//assignment and evaluation in 1 line as opposed to nested ifs
+			//value is true, we want to show
+			if (value is bool isboolValue && isboolValue)
+			{
+				return Visibility.Visible;
+			}
+			//value was false, collapse
+			else
+			{
+				return Visibility.Hidden;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ YetAnotherYTDLDownloader/Converters/BoolToVisiblityInverted.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace YetAnotherYTDLDownloader.Converters
+{
+	/// <summary>
+	/// Returns visible if the boolean is false
+	/// and returns hidden if boolean is true
+	/// </summary>
+	internal class BoolToVisiblityInverted : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			//value was true, collapse
+			if (value is bool isboolValue && isboolValue)
+			{
+				return Visibility.Hidden;
+			}
+			//value was false, go visible
+			else
+			{
+				return Visibility.Visible;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
